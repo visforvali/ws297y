89cfc4e160f98464a9bec6eebaf21d58a5990c05
v is for வலி <53793009+visforvali@users.noreply.github.com>
2025-08-14 22:07:48 -0400

    Deleted assets js

--- assets/js/global-vars.js
+++
@@ -1,3 +0,0 @@
-var last_Savedmode;
-var theme = localStorage.getItem('theme');
-var theme_html = document.documentElement.getAttribute('data-theme');
--- assets/js/toggle_init.js
+++
@@ -1,8 +0,0 @@
-const theme = localStorage.getItem('theme');
-if (theme === "dark") {
-  document.documentElement.setAttribute('data-theme', 'dark');
-}
-else
-{
-  document.documentElement.setAttribute('data-theme', 'light');
-}
